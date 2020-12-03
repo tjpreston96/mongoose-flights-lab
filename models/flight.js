53f48d58af25695806ad55098e3dd7a85e1d0659
tpreston96 <tpreston96@gmail.com>
2020-12-03 14:59:09 -0600

    framing tickets

--- models/flight.js
+++ models/flight.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
+const ticketSchema = new Schema ({
+    seat: {type: String, match: /[A-F][1-9]\d?/},
+    price: {type: Number, min: 0}
+},{
+    timestamps: true
+  })
+
 const flightSchema = new Schema ({
     airline: {type:  String, enum: ['American','Southwest','United','Spirit','Delta']},
     airport: {type:  String, enum: ['AUS','DFW','DEN','LAX', 'SAN'], default: 'DEN'},
@@ -13,6 +20,8 @@ const flightSchema = new Schema ({
     departs: { type: Date,
      default: () => Date.now() + 365 * 24 * 60 * 60 * 1000
     }
-});
+}, {
+    timestamps: true
+  });
 
 module.exports = mongoose.model('Flight', flightSchema)
