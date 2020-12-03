457ec9b7ba3407f79edfdf658f74104780ca03d4
tpreston96 <tpreston96@gmail.com>
2020-12-02 22:05:45 -0600

    fixed schema --> WORKS!

--- models/flight.js
+++ models/flight.js
@@ -4,7 +4,12 @@ const Schema = mongoose.Schema
 const flightSchema = new Schema ({
     airline: {type:  String, enum: ['American','Southwest','United','Spirit','Delta']},
     airport: {type:  String, enum: ['AUS','DFW','DEN','LAX', 'SAN'], default: 'DEN'},
-    flightNo: Number,
+    flightNo: {
+    type: Number,
+    required: true,
+    min: 10,
+    max: 9999
+    },
     departs: { type: Date,
      default: function() {
         return new Date().getFullYear()+1
